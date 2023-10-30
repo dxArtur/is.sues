c107d28ca6bd07e6ec37a1923ff041599e4509e4
dxArtur <daniel.arturjsb@gmail.com>
2023-10-30 16:04:10 -0300

    updates front-end

--- src/client/app/profile/page.tsx
+++ src/client/app/profile/page.tsx
@@ -1,32 +1,71 @@
-import React from "react"
-import { Avatar, Text, Button, Flex } from "@radix-ui/themes"
+'use client'
+
+import React, { useState } from "react"
+import { Avatar, Text, Button, Flex, Separator, Strong } from "@radix-ui/themes"
+import classnames from 'classnames'
+import { RiTeamLine } from 'react-icons/ri'
+import { CgDanger } from 'react-icons/cg'
+import { MdOutlineModeEditOutline } from 'react-icons/md'
 
 
 const profilePage =()=>{
 
+    const sessionUser = 
+    {
+        id: '1a2b3c4d',
+        name: "Maria da Silva",
+        email: "maria@example.com",
+        idDepartament: 1, // TI
+        occupation: "Desenvolvedor",
+        photo: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fHww"
+      }
+
+        
+    const [isHovered, setIsHovered] = useState(false)
     return(
-        <div className="min-h-screen bg-gray-100 py-6 flex flex-col items-center justify-center">
-      <div className="max-w-md w-full space-y-4">
-        <Avatar
-          src="https://via.placeholder.com/150"
-          alt="Profile Picture"
-          size={150}
-          className="mx-auto"
-        />
-        <Text size="2xl" weight="bold" className="text-center">
-          John Doe
-        </Text>
-        <Text size="lg" className="text-center">
-          Software Developer
-        </Text>
-        <Flex direction="row" gap={1} className="justify-center">
-          <Button as="a" href="#" variant="blue">
-            Follow
-          </Button>
-          <Button variant="white">Message</Button>
-        </Flex>
-      </div>
-    </div>
+        <div className="space-y-3">
+            <div className="flex items-center">
+                <Avatar
+                    src={sessionUser.photo}
+                    size={'9'}
+                />
+                <div className=" p-4 max-w-4xl border border-zinc-500 rounded-md ml-4">
+                    <Text as="p" size={'4'}>
+                        <Strong>
+                            {sessionUser.name}
+                        </Strong>
+                    </Text>
+                    <Text size={'3'}>
+                        {sessionUser.occupation}
+                    </Text>
+                </div>
+            </div>
+
+            <div className=" space-y-3 ">
+                <Button className={classnames({
+                    'hover:text-red-500  border-4 border-red-500': true 
+                })}
+                variant="soft"
+                color="gray"
+                onMouseEnter={()=> setIsHovered(true)}
+                onMouseLeave={()=> setIsHovered(false)}
+                >
+                    { isHovered ?
+                    <>
+                    <CgDanger />  {'Danger zone'}
+                    </>
+                    :
+                    <>
+                    <MdOutlineModeEditOutline /> {'Edit profile'}
+                    </>}
+                </Button>
+                <Separator  color="red"/>
+                <div className="items-center flex text-zinc-500 space-x-2">
+                    <RiTeamLine />is a member of departament {sessionUser.idDepartament}
+                </div>
+                <Button>Send me a email</Button>
+            </div>
+        </div>
     )
 }
 
