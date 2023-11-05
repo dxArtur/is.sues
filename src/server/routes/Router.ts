7267c36800149e50c910db2463a48d21704f4ef1
Israel <israeloliveira171@gmail.com>
2023-11-05 16:46:36 -0300

    CRUD DEPARTMENTS

--- src/server/routes/Router.ts
+++ src/server/routes/Router.ts
@@ -2,10 +2,14 @@ import { Router } from 'express'
 import userRouter from './users-routes'
 import issueRouter from './issues-router'
 import seedRoutes from './seed-route-label'
+import departamentRoutes from './department-routes'
+
+
 const router = Router()
 
 router.use('/', userRouter)
 router.use('/', issueRouter)
 router.use('/', seedRoutes)
+router.use('/', departamentRoutes)
 
 export default router
