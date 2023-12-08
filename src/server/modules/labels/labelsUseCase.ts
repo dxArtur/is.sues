72f346f1b8833c230fd570d10815a451c38d31d0
Israel <israeloliveira171@gmail.com>
2023-12-07 22:18:39 -0300

    Adicionando validação de campos Labels

--- src/server/modules/labels/labelsUseCase.ts
+++ src/server/modules/labels/labelsUseCase.ts
@@ -1,64 +1,61 @@
 import { prisma } from "../../database/repositoryClient"
 import utilsCrypt from '../../utils/crypt'
+import { CreateLabelDto, GetLabelByIdDTO, UpdateLabelDto } from "../../../dtos/LabelDTO"
 import { sign } from 'jsonwebtoken';
 
 export class LabelUseCase{
-    async createLabel({ name, description, departmentId }){
+    async createLabel(labelData: CreateLabelDto) {
         try {
             const newLabel = await prisma.label.create({
                 data: {
-                  name,
-                  description,
-                  departmentId,
+                    name: labelData.name,
+                    description: labelData.description,
+                    department: { connect: { id: labelData.departmentId } }
                 }
-            })
-            return newLabel
-        } catch (error) {
-           throw new Error(error) 
-        }
-    }
-
-    async getLabelById({ id }) {
-        try {
-            const label = await prisma.label.findUnique({
-                where: { id: id },
             });
 
-            return label
+            return newLabel;
         } catch (error) {
-            throw new Error(error) 
+            throw new Error("Erro ao criar o label.");
         }
     }
 
-    
-    async updateLabel({ id, name, description, departmentId }) {
+    async getLabelById({ id }: GetLabelByIdDTO) {
+        try {
+            const label = await prisma.label.findUnique({
+                where: { id },
+            });
+
+            return label;
+        } catch (error) {
+            throw new Error("Erro ao buscar o label.");
+        }
+    }
+    async updateLabel(labelData: UpdateLabelDto) {
         try {
             const updatedLabel = await prisma.label.update({
-                where: {
-                    id: id
-                },
+                where: { id: labelData.id },
                 data: {
-                    name,
-                    description,
-                    departmentId, 
+                    ...(labelData.name && { name: labelData.name }),
+                    ...(labelData.description && { description: labelData.description }),
+                    ...(labelData.departmentId && { department: { connect: { id: labelData.departmentId } } })
                 }
-            })
-            return updatedLabel
+            });
+
+            return updatedLabel;
         } catch (error) {
-            throw new Error(error) 
+            throw new Error("Erro ao atualizar o label.");
         }
     }
 
-    async deleteLabel({id}) {
+    async deleteLabel({ id }: { id: number }) {
         try {
             const deletedLabel = await prisma.label.delete({
-                where: {
-                    id: id
-                }
-            })
-            return deletedLabel
+                where: { id }
+            });
+            return deletedLabel;
         } catch (error) {
-            throw new Error(error) 
+            throw new Error("Erro ao deletar o label.");
         }
     }
     
@@ -67,7 +64,7 @@ export class LabelUseCase{
             const allLabels = await prisma.label.findMany({ })
             return allLabels
         } catch (error) {
-            throw new Error(error) 
+            throw new Error("Erro ao buscar labels.");
         }
     }
 }
